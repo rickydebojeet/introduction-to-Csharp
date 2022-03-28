cb76f7c3bbb75219fe2267074eca864dcf4cc510
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2022-03-28 22:11:57 +0530

    added fifth programming excercise

---
+++ Chapter_3/3_5.cs
@@ -0,0 +1,14 @@
+// Pattern Generator
+using syc = System.Console;
+
+class patternGenerator
+{
+    public static void Main()
+    {
+        syc.WriteLine(" " + " " + " " + " " + "X");
+        syc.WriteLine(" " + " " + "X" + " " + "X" + " " + "X");
+        syc.WriteLine("X" + " " + "X" + " " + "X" + " " + "X" + " " + "X");
+        syc.WriteLine(" " + " " + "X" + " " + "X" + " " + "X");
+        syc.WriteLine(" " + " " + " " + " " + "X");
+    }
+}
