70f6ae322bdc0e96c8976642c6a61a58a64b906f
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2022-03-29 18:35:53 +0530

    updated with moduarity

--- Chapter_3/3_5.cs
+++ Chapter_3/3_5.cs
@@ -5,10 +5,40 @@ class patternGenerator
 {
     public static void Main()
     {
-        syc.WriteLine(" " + " " + " " + " " + "X");
-        syc.WriteLine(" " + " " + "X" + " " + "X" + " " + "X");
-        syc.WriteLine("X" + " " + "X" + " " + "X" + " " + "X" + " " + "X");
-        syc.WriteLine(" " + " " + "X" + " " + "X" + " " + "X");
-        syc.WriteLine(" " + " " + " " + " " + "X");
+        int i, j, a, b;
+        for (i = 4, j = 1; i >= 0 && j <= 5; i -= 2, j += 2)
+        {
+            a = i;
+            b = j;
+            while (a > 0)
+            {
+                syc.Write(" ");
+                a--;
+            }
+            while (b > 0)
+            {
+                syc.Write("X ");
+                b--;
+            }
+            syc.WriteLine("");
+        }
+        i += 4;
+        j -= 4;
+        for (; i <= 4 && j >= 0; i += 2, j -= 2)
+        {
+            a = i;
+            b = j;
+            while (a > 0)
+            {
+                syc.Write(" ");
+                a--;
+            }
+            while (b > 0)
+            {
+                syc.Write("X ");
+                b--;
+            }
+            syc.WriteLine("");
+        }
     }
 }
