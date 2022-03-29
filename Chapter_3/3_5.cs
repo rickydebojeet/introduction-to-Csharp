ff1af00d87cc4207ba6638292192770846a0a3c9
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2022-03-29 20:11:32 +0530

    added option to scale the pattern

--- Chapter_3/3_5.cs
+++ Chapter_3/3_5.cs
@@ -5,8 +5,14 @@ class patternGenerator
 {
     public static void Main()
     {
-        int i, j, a, b;
-        for (i = 4, j = 1; i >= 0 && j <= 5; i -= 2, j += 2)
+        int i, j, a, b, spaceLimit;
+        syc.Write("Enter the number of max X in the pattern (in odd): ");
+        int choice = System.Convert.ToInt32(syc.ReadLine());
+
+        spaceLimit = 2 * (choice / 2);
+        i = spaceLimit;
+        j = 1;
+        while (i >= 0 && j <= choice)
         {
             a = i;
             b = j;
@@ -21,10 +27,12 @@ class patternGenerator
                 b--;
             }
             syc.WriteLine("");
+            i -= 2;
+            j += 2;
         }
         i += 4;
         j -= 4;
-        for (; i <= 4 && j >= 0; i += 2, j -= 2)
+        while (i <= spaceLimit && j >= 0)
         {
             a = i;
             b = j;
@@ -39,6 +47,8 @@ class patternGenerator
                 b--;
             }
             syc.WriteLine("");
+            i += 2;
+            j -= 2;
         }
     }
 }
