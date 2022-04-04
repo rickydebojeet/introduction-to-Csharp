f1f62f6e31ce24516c8b362d106ffe595886a292
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2022-04-04 17:51:54 +0530

    Create Program1.cs
    Added counting with if Statement

---
+++ Tests/Chapter_6/Program1.cs
@@ -0,0 +1,27 @@
+// Counting with if statement
+
+using System;
+class count
+{
+    public static void Main()
+    {
+        int count, count1, count2;
+        float[] weight = { 45.0F, 55.0F, 47.0F, 51.0F, 54.0F };
+        float[] height = { 176.5F, 174.2F, 168.0F, 170.7F, 169.0F };
+        count = 0;
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i <= 4; i++)
+        {
+            if (weight[i] < 50.0 && height[i] > 170.0)
+            {
+                count1++;
+            }
+            count++;
+        }
+
+        count2 = count - count1;
+        Console.WriteLine("The number of students fullfilling the condition is : " + count1);
+        Console.WriteLine("The number of students who doesn't fullfill the condition is : " + count2);
+    }
+}
