42dd67601e059d63be570d8ee1ef91d7a79978e3
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2022-04-04 17:58:31 +0530

    Create Program3.cs
    Program that uses boolean Variable

---
+++ Tests/Chapter_6/Program3.cs
@@ -0,0 +1,14 @@
+// Using Boolean Variable
+
+class booleanTest
+{
+    public static void Main()
+    {
+        int a = 10, b = 20, c = 10;
+        bool d, e;
+        d = a > b;
+        e = a == c;
+        System.Console.WriteLine(d);
+        System.Console.WriteLine(e);
+    }
+}
