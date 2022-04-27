25ebd2dc0bafe94337e69970f58f5c2f9e1e4c44
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2022-04-28 00:25:23 +0530

    Added Comments

--- Tests/Chapter_3/Program5.cs
+++ Tests/Chapter_3/Program5.cs
@@ -1,20 +1,19 @@
 // A program with more than one class
-
-using D = System.Console;
-
-class A
+using syc = System.Console;
+// Class Definition
+class testClass
 {
     public void fun()
     {
-        D.WriteLine("C# is Modern");
+        syc.WriteLine("C# is Modern");
     }
 }
 
-class B
+class program5
 {
     public static void Main()
     {
-        A a = new A();
-        a.fun();
+        testClass ob1 = new A(); // Creating 'ob1' object
+        ob1.fun(); // calling fun() function
     }
 }
