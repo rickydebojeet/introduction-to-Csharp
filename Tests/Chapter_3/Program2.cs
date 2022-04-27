28a2477cd71edb06b0dd31b8632d079339b72fc2
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2022-04-27 23:10:06 +0530

    Added comments

--- Tests/Chapter_3/Program2.cs
+++ Tests/Chapter_3/Program2.cs
@@ -1,10 +1,12 @@
 // Using Aliasing
 
-using A = System.Console;
+// Here syc is an alias of System.Console
+using syc = System.Console;
 class program2
 {
     public static void Main()
     {
-        A.WriteLine("CITK");
+        // The line interprets to System.Console.WriteLine("CITK");
+        syc.WriteLine("CITK");
     }
 }
