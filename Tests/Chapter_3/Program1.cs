0fc7a50294378d55dbd4455696341a4c740f2290
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2022-04-27 21:51:58 +0530

    Added Comments

--- Tests/Chapter_3/Program1.cs
+++ Tests/Chapter_3/Program1.cs
@@ -1,8 +1,10 @@
 // A Simple C# Program
 class program1
 {
+    // These is where the program starts executing the code
     public static void Main()
     {
+        // Printing the string in the console
         System.Console.WriteLine("C# is sharper than C++");
     }
 }
