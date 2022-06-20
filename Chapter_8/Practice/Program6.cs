0c5d565013cc56aacb93fe29b5be652a7f7c89e5
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2022-06-20 18:22:56 +0530

    ⚡ Updated code to basic example

--- Chapter_8/Practice/Program6.cs
+++ Chapter_8/Practice/Program6.cs
@@ -1,37 +1,16 @@
-//* Use output Parameter to find the evenSum and oddSum of integers between 50 to 100
+// Defining an output parameter
+using syc = System.Console;
 
-using System;
-
-class sumCalculator
+class Output
 {
-    static void even(out int evenSum)
+    static void Square(int x, out int y)
     {
-        evenSum = 0;
-        for (int i = 50; i <= 100; i++)
-        {
-            if ((i % 2) == 0)
-            {
-                evenSum += i;
-            }
-        }
-    }
-    static void odd(out int oddSum)
-    {
-        oddSum = 0;
-        for (int i = 50; i <= 100; i++)
-        {
-            if ((i % 2) != 0)
-            {
-                oddSum += i;
-            }
-        }
+        y = x * x;
     }
     public static void Main()
     {
-        int evenSum = 0, oddSum = 0;
-        even(out evenSum);
-        Console.WriteLine("Even Sum = " + evenSum);
-        odd(out oddSum);
-        Console.WriteLine("Odd Sum = " + oddSum);
+        int m; // need not be iniitialized
+        Square(10, out m);
+        syc.WriteLine("m = " + m);
     }
 }
